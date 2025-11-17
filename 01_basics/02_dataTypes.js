27d09fb6d9814749d952c3d7d8a716e9cae91c63
CoderBCAwala <coderbcawala99@gmail.com>
2025-11-17 05:28:35 +0000

    Data Type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat  all js code as newer versions
+
+// alert(3+3); we are using nodejs not browser
+
+console.log(3+3); 
+console.log("hello world");
+
+let name = "charan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to 53
+// bigint => more than 53
+// string => "" , '' , ``
+// boolean => true/false
+// null => standalone value 
+// undefined => standalone value
+// symbol => unique
+
+
+
+// object => key value pairs
+console.log(typeof null);
+console.log(typeof undefined);
