57b44605a14e7c1b277549332777406e290a4262
CoderBCAwala <coderbcawala99@gmail.com>
2025-11-18 02:59:02 +0000

    Read Again Conversion & Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,5 +20,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log( typeof stringNumber)
+// console.log(stringNumber)
+// console.log( typeof stringNumber)
+
+// ************  Operations  **************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3) // 2^3=8
+// console.log(7%3) // remainder
+
+
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+// console.log(str3) // hello world
+
+// console.log( "1"+2) // "1"+2=>"12"
+// console.log( 1+"2")
+// console.log( "1"+ 2 +2) 
+// console.log( 1+ 2 + "3")
+
+
+
+// console.log(+ true)
+
