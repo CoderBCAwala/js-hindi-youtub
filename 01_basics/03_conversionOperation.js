cea900b5a44ca2c141f0faf81619305623ec09fc
CoderBCAwala <coderbcawala99@gmail.com>
2025-11-18 02:38:15 +0000

    Conversion DataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+
+// console.log (typeof score); // number
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log (typeof valueInNumber); // number
+// console.log (valueInNumber); // number
+
+
+//   "33"=>33
+//   "33abc"=>NaN
+//   true => 1
+//   false =>0
+
+let isLoggedIn = 3
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
