d6704fb0a19601635c33c03636bfe55bb2542205
CoderBCAwala <coderbcawala99@gmail.com>
2025-11-19 14:49:57 +0000

    For interview sumary

---
+++ 01_basics/05_DataTypeSummry.js
@@ -0,0 +1,26 @@
+//Primitive Data type
+
+// string, number, boolean, null, undefined, symbol, bigint,
+
+// non-primitive(Reference)
+//  Arry, object, Function
+
+// JavaScript is 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol("123")
+
+const heros = ["shaktiman", "naagraj", "doga"]
+const myObj = {
+    name:"charan",
+    age:18,
+}
+
+const myFunction = function(){
+    console.log("hello")
+}
+console.log(typeof myFunction)
